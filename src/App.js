aff4d0c29b68eb367ffb0d084303caa58abe4e0b
hemilrajpura-28 <iamhemilsoni@gmail.com>
2022-11-08 17:20:13 +0530

    added alt

--- src/App.js
+++ src/App.js
@@ -1,36 +1,42 @@
-import './App.css'; 
+import "./App.css";
 import { motion } from "framer-motion";
 
-const App = ()=>{
+const App = () => {
   return (
-    <div className='App'>
-      
-    <motion.div animate={{
-      x:0,
-      scale:1
-    }} initial={{
-      x:-100,
-      scale:0.5
-    }}>
-      <img
-      height={50}
-      width={50}
-      src="https://www.gizchina.com/wp-content/uploads/images/2022/10/macOS-6.jpg" />
-    </motion.div>
+    <div className="App">
+      <motion.div
+        animate={{
+          x: 0,
+          scale: 1,
+        }}
+        initial={{
+          x: -100,
+          scale: 0.5,
+        }}
+      >
+        <img
+          alt="some"
+          height={50}
+          width={50}
+          src="https://www.gizchina.com/wp-content/uploads/images/2022/10/macOS-6.jpg"
+        />
+      </motion.div>
 
-    <motion.div drag="x"
-    dragConstraints={{
-      left:10,
-      right:10
-    }}
-    >
-    <img
-      height={50}
-      width={50}
-      src="https://e7.pngegg.com/pngimages/327/99/png-clipart-computer-icons-pointer-drag-and-drop-symbol-full-screen-miscellaneous-angle.png"/>
-    </motion.div>
-
+      <motion.div
+        drag="x"
+        dragConstraints={{
+          left: 10,
+          right: 10,
+        }}
+      >
+        <img
+          height={50}
+          width={50}
+          alt="some"
+          src="https://e7.pngegg.com/pngimages/327/99/png-clipart-computer-icons-pointer-drag-and-drop-symbol-full-screen-miscellaneous-angle.png"
+        />
+      </motion.div>
     </div>
   );
-}
+};
 export default App;
